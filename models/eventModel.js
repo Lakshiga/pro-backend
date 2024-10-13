4fa380a152e1edffbdff4e7da3a7067a9ccc63ba
Lakshiga <lakshiga20021216@gmail.com>
2024-10-13 23:28:29 +0530

    event model change

--- models/eventModel.js
+++ models/eventModel.js
@@ -8,6 +8,9 @@ const eventSchema = new Schema({
   matchType: { type: String, enum: ['knockout', 'league'], required: true },
   players: [{ type: String, required: true }], // Array of player names
   matches: [{ type: Schema.Types.ObjectId, ref: 'Match' }],
+  organizer_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  umpire_ids: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  status: { type: String, enum: ["active", "completed"], default: "active" },
 });
 
 export default model("Event", eventSchema);
