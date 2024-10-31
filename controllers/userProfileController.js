ae12ee83f1550298aa2f94299d351186c1ae8750
Lakshiga <lakshiga20021216@gmail.com>
2024-10-31 19:46:24 +0530

    useProfileController update

--- controllers/userProfileController.js
+++ controllers/userProfileController.js
@@ -30,3 +30,31 @@ export const updateUserProfile = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+export const getPlayersByIds = async (req, res) => {
+  const { playerIds } = req.body;
+
+  try {
+    const players = await User.find({
+      _id: { $in: playerIds },
+      role: "player",
+    });
+
+    if (!players.length) {
+      return res.status(404).json({ message: "No players found" });
+    }
+    
+    const playerDetails = players.map((player) => ({
+      id: player._id,
+      username: player.username,
+      email: player.email,
+      isVerified: player.isVerified,
+      role: player.role,
+    }));
+
+    res.json(playerDetails);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -3,11 +3,13 @@ import  {protect}  from "../middleware/authMiddleware.js";
 import {
   getUserProfile,
   updateUserProfile,
+  getPlayersByIds,
 } from "../controllers/userProfileController.js";
 
 const router = Router();
 
 router.get("/profile", protect, getUserProfile); // Get current user profile
 router.put("/profile", protect, updateUserProfile); // Update user profile
+router.post("/list", getPlayersByIds);
 
 export default router;
